86699b0478298510b25b3cd3e82cc044cd91c8d8
Stephen Lester <slester@startmail.com>
2019-02-03 19:42:17 -0800

    mix format

--- lib/Advent2018/Day1.ex
+++ lib/Advent2018/Day1.ex
@@ -12,6 +12,7 @@ defmodule Advent2018.Day1 do
     def execute(input) do
       freq_changes = input |> Enum.map(&String.to_integer/1)
       seen_freqs = MapSet.new([0])
+
       find_repetition(freq_changes, 0, seen_freqs)
       |> Integer.to_string()
     end
@@ -19,6 +20,7 @@ defmodule Advent2018.Day1 do
     defp find_repetition(freq_changes, current_freq, seen_freqs) do
       [change | rest_changes] = freq_changes
       new_freq = current_freq + change
+
       if MapSet.member?(seen_freqs, new_freq) do
         new_freq
       else
